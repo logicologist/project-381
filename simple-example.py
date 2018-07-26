24d91584e36210b14e4ea5d2fe361b442b546e53
logicologist <kcr32@uw.edu>
2018-07-25 18:41:09 -0700

    Refactored to make stats easier to compute

--- simple-example.py
+++ simple-example.py
@@ -7,17 +7,17 @@ random.seed(100)
 r_dim = 4
 c_dim = 5
 
+# Constants
+d = 5 # number of days to run simulation
+p = 0.25 # probability of transmission to adjacent student
+
 # 0 = uninfected, 1 = sick
-room = [[0 for j in range(c_dim)] for i in range(r_dim)]
+room_log = [[[0 for j in range(c_dim)] for i in range(r_dim)] for t in range(d+1)]
 
 # Pick one student at random to catch the flu
 
 source = (random.randrange(0, r_dim), random.randrange(0, c_dim))
-room[source[0]][source[1]] = 1
-
-# Constants
-d = 5 # number of days to run simulation
-p = 0.25 # probability of transmission to adjacent student
+room_log[0][source[0]][source[1]] = 1
 
 def pretty_print(room):
     '''Generates printable string of room layout and infection status'''
@@ -30,14 +30,15 @@ def pretty_print(room):
 
 # Simulation
 print("t = 0:")
-print(pretty_print(room))
+print(pretty_print(room_log[0]))
 
+current_room = room_log[0]
 for t in range(1, d+1):
     # Get state of each seat for time step t
-    new_room = [[0 for j in range(c_dim)] for i in range(r_dim)]
+    new_room = room_log[t]
     for row in range(r_dim):
         for col in range(c_dim):
-            if room[row][col] == 0: # student uninfected
+            if current_room[row][col] == 0: # student uninfected
                 num_sick = 0
                 # count sick neighbors
                 for dr in (-1, 0, 1):
@@ -47,7 +48,7 @@ for t in range(1, d+1):
                         if (dr, dc) != (0, 0) \
                                 and row + dr >= 0 and row + dr < r_dim \
                                 and col + dc >= 0 and col + dc < c_dim \
-                                and room[row + dr][col + dc] == 1:
+                                and current_room[row + dr][col + dc] == 1:
                             num_sick += 1
                 if num_sick > 0:
                     # Calculate transmission probability
@@ -59,8 +60,21 @@ for t in range(1, d+1):
                 new_room[row][col] = 1
     
     # Update room to this new state
-    room = [[new_room[r][c] for c in range(c_dim)] for r in range(r_dim)]
+    current_room = room_log[t]
     
     # Print result
     print("t = " + str(t) + ":")
-    print(pretty_print(room))
+    print(pretty_print(room_log[t]))
+
+
+# Stats
+
+# Average number of days uninfected
+uninfected_days = 0;
+for row in range(r_dim):
+    for col in range(c_dim):
+        history = [room_log[t][row][col] for t in range(d+1)]
+        uninfected_days += history.count(0)
+uninfected_days /= (r_dim * c_dim) # average over total number of students
+print("Average number of days uninfected: " + str(uninfected_days))
+
