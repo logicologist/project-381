12b817741f5310a9e5b5e1b79f52d5de295d23e4
logicologist <kcr32@uw.edu>
2018-08-07 19:06:24 -0700

    Plotting number of days spent sick, averaged across multiple trials.

--- monte_carlo_sim.py
+++ monte_carlo_sim.py
@@ -8,6 +8,8 @@ import itertools as itr
 from Student import *
 from classes import classes
 
+r.seed(42) # for reproducability
+
 def update_states(room, infect_rate, day, weekends = False):
     shape = np.shape(room)
     for row in range(shape[0]):
@@ -138,11 +140,9 @@ def graph_frac_infected(classrooms_list, num_days):
         for i, cs in enumerate(classrooms):
             end_results = cs[-1, :, :]
             counts = list()
-            days_sick = list()
             for row in end_results:
                 for student in row:
                     counts = counts + student.days_infected
-                    days_sick += [len(student.days_infected)]
     
             class_shape = np.shape(end_results)
             class_size = class_shape[0]*class_shape[1]
@@ -163,6 +163,33 @@ def graph_frac_infected(classrooms_list, num_days):
     plt.ylabel("Fraction Classroom Infected")
     plt.xlabel("Day")
     plt.xticks(list(range(0, num_days, 5)))
+
+
+def graph_days_infected(classrooms_list, num_days):
+    ''' Graphs the distribution of the number of days that students are
+    infected with the flu. Params:
+        classrooms_list[which_trial][which_classroom][which_time_step][row][column]
+        num_days: number of time steps'''
+    n_trials = len(classrooms_list)
+    
+    days_sick = list()
+    for trial, classrooms in enumerate(classrooms_list):
+        for i, cs in enumerate(classrooms):
+            end_results = cs[-1, :, :]
+            for row in end_results:
+                for student in row:
+                    days_sick += [len(student.days_infected)]
+    
+    bin_width = 5
+    edges = list(range(0, num_days + 1, bin_width))
+    fig, ax = plt.subplots()
+    plt.hist(days_sick, bins=edges, rwidth=0.9)
+    y_vals = ax.get_yticks()
+    ax.set_yticklabels(['{:1.0f}'.format(x // n_trials) for x in y_vals])
+    plt.title("Number of days spent sick (averaged across all trials)")
+    plt.xlabel("Days Spent Sick")
+    plt.ylabel("# Students")
+    plt.xticks(edges)
 
 
 # runs a single simulation for the spread of the flu
@@ -234,6 +261,8 @@ for trial in range(trials):
 
 plt.figure(1)
 graph_frac_infected(classrooms_list, time_steps)
+plt.figure(2)
+graph_days_infected(classrooms_list, time_steps)
 
 plt.show()
 
