c98da6d784437dfe4e98b126875f2e463992e777
logicologist <kcr32@uw.edu>
2018-08-07 18:04:39 -0700

    Getting code to collect data from multiple trials. Averaging fraction of classroom infected at each time step over all trials.

--- monte_carlo_sim.py
+++ monte_carlo_sim.py
@@ -123,6 +123,47 @@ def graph_results(classrooms, num_days):
 
     plt.show()
 
+def graph_frac_infected(classrooms_list, num_days):
+    ''' Graphs the average fraction of classroom infected for each classroom,
+    averaged over all trials run. Params:
+        classrooms_list[which_trial][which_classroom][which_time_step][row][column]
+        num_days: number of time steps'''
+    t_vals = list(range(num_days))
+    # Data: frac_sick_data[which_trial][which_classroom]
+    frac_sick_data = list()
+    
+    # Compute fraction of students sick for each trial for each classroom
+    for trial, classrooms in enumerate(classrooms_list):
+        frac_sick_data.append(list())
+        for i, cs in enumerate(classrooms):
+            end_results = cs[-1, :, :]
+            counts = list()
+            days_sick = list()
+            for row in end_results:
+                for student in row:
+                    counts = counts + student.days_infected
+                    days_sick += [len(student.days_infected)]
+    
+            class_shape = np.shape(end_results)
+            class_size = class_shape[0]*class_shape[1]
+            frac_sick = [counts.count(x) * 1.0 / class_size for x in t_vals]
+            frac_sick_data[trial].append(frac_sick)
+            
+    # Compute average fraction of students sick for each classroom across all trials
+    n_trials = len(frac_sick_data)
+    for classroom_index in range(len(frac_sick_data[0])):
+        avg_frac_sick = []
+        for t in range(num_days):
+            frac_sick_t = [frac_sick_data[trial][classroom_index][t] for trial in range(n_trials)]
+            avg_frac_sick.append(sum(frac_sick_t) / len(frac_sick_t))
+        plt.plot(t_vals, avg_frac_sick, label="Classroom " + str(classroom_index+1)) # + " shape: " + str(class_shape))
+
+    # Label figure
+    plt.legend(loc='best')
+    plt.ylabel("Fraction Classroom Infected")
+    plt.xlabel("Day")
+    plt.xticks(list(range(0, num_days, 5)))
+
 
 # runs a single simulation for the spread of the flu
 # preconditions: no classroom has more than 1 / classes_per_student of the total number
@@ -172,15 +213,29 @@ def run_simulation(infect_rate, class_sizes, time_steps, classes_per_student = 1
             print("Classroom: " + str(i + 1))
             print(str(results[day, :, :]) + "\n")
 
-    graph_results(classrooms, time_steps)
+    return classrooms
 
 
 # classroom dimensions: each tuple = 1 classroom (rows, columns)
 class_sizes = [(5, 5), (10, 10)]
 
+trials = 5 # number of times to run simulation
 time_steps = 100  # days to run simulation for
 infection_rate = 0.1 # chance per sick neighbor of spreading infection
 recovery_time_fixed_days = 8 # constant number of days that an infected student is sick at minimum
 recovery_time_dropoff_rate = 0.5 # after fixed days, student recovers with this probability each day
 
-run_simulation(infection_rate, class_sizes, time_steps, weekends=False)
+classrooms_list = []
+for trial in range(trials):
+    classrooms = run_simulation(infection_rate, class_sizes, time_steps, weekends=False)
+    classrooms_list.append(classrooms)
+    # What the classrooms data structure looks like:
+    # classrooms[which_classroom][which_time_step][row][column]
+
+plt.figure(1)
+graph_frac_infected(classrooms_list, time_steps)
+
+plt.show()
+
+#graph_results(classrooms_list, time_steps)
+
